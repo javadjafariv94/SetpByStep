08f33755d31d6eee2a23e4cdf0aba6e8eec8faec
javadjafariv94 <javadjafariv94@gmail.com>
2019-02-09 20:05:02 +0330

    alignItem

--- Desktop/StepByStep/App.js
+++ Desktop/StepByStep/App.js
@@ -6,18 +6,22 @@ export default class App extends Component {
     
     return (
        //flexDirection deafult is : column
+       
        //justifyContent :flex-start, center, flex-end, 
        //space-around, space-between and space-evenly.
 
+       //alignItems : flex-start, center, flex-end, and stretch.
+
        <View style = {{
          flex :1,
-         flexDirection : 'row',
-         justifyContent : 'space-between',
+         flexDirection : 'column',
+         justifyContent : 'center',
+         alignItems : 'stretch',
          }}> 
      
             <View style = {{height : 50,width:50 , backgroundColor: 'powderblue'}}/>
-            <View style = {{height : 50,width:50 , backgroundColor: 'skyblue'}}/>
-            <View style = {{height : 50,width:50, backgroundColor: 'steelblue'}}/>
+            <View style = {{height : 50, backgroundColor: 'skyblue'}}/>
+            <View style = {{height : 50, backgroundColor: 'steelblue'}}/>
      </View>
     );
   }
