6f479ad9a65f100c1513b381e21a9460840394fd
javadjafariv94 <javadjafariv94@gmail.com>
2019-02-09 19:28:16 +0330

    flex

--- Desktop/StepByStep/App.js
+++ Desktop/StepByStep/App.js
@@ -3,12 +3,14 @@ import { View,Text,StyleSheet} from 'react-native';
 
 export default class App extends Component {
  render(){
-    
+    // in flex down 1+3+2=6  screen size /6
     return (
-      <View>
-            <View style = {{width : 50  ,height :50 , backgroundColor: 'powderblue'}}/>
-            <View style = {{width : 100 ,height :100 ,backgroundColor: 'skyblue'}}/>
-            <View style = {{width : 150 ,height :150 ,backgroundColor: 'steelblue'}}/>
+       // parent with flex : 1 all screen size selected
+       <View style = {{flex :1}}> 
+      {/* <View style = {{height:300}}> */}
+            <View style = {{flex:1 , backgroundColor: 'powderblue'}}/>
+            <View style = {{flex:3 , backgroundColor: 'skyblue'}}/>
+            <View style = {{flex:2 , backgroundColor: 'steelblue'}}/>
      </View>
     );
   }
