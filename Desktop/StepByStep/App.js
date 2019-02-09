67547e8eff696d39a7ae2c12886acbc225617269
javadjafariv94 <javadjafariv94@gmail.com>
2019-02-09 19:44:26 +0330

    flexDirection

--- Desktop/StepByStep/App.js
+++ Desktop/StepByStep/App.js
@@ -3,14 +3,14 @@ import { View,Text,StyleSheet} from 'react-native';
 
 export default class App extends Component {
  render(){
-    // in flex down 1+3+2=6  screen size /6
+    
     return (
-       // parent with flex : 1 all screen size selected
-       <View style = {{flex :1}}> 
-      {/* <View style = {{height:300}}> */}
-            <View style = {{flex:1 , backgroundColor: 'powderblue'}}/>
-            <View style = {{flex:3 , backgroundColor: 'skyblue'}}/>
-            <View style = {{flex:2 , backgroundColor: 'steelblue'}}/>
+       //flexDirection deafult is : column
+       <View style = {{flex :1 , flexDirection : 'row'}}> 
+     
+            <View style = {{height : 50,width:50 , backgroundColor: 'powderblue'}}/>
+            <View style = {{height : 50,width:50 , backgroundColor: 'skyblue'}}/>
+            <View style = {{height : 50,width:50, backgroundColor: 'steelblue'}}/>
      </View>
     );
   }
