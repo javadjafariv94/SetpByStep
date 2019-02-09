988f12a8dd462adaf61043185874b35a28934e2a
javadjafariv94 <javadjafariv94@gmail.com>
2019-02-09 19:55:27 +0330

    justyifyContent

--- Desktop/StepByStep/App.js
+++ Desktop/StepByStep/App.js
@@ -6,7 +6,14 @@ export default class App extends Component {
     
     return (
        //flexDirection deafult is : column
-       <View style = {{flex :1 , flexDirection : 'row'}}> 
+       //justifyContent :flex-start, center, flex-end, 
+       //space-around, space-between and space-evenly.
+
+       <View style = {{
+         flex :1,
+         flexDirection : 'row',
+         justifyContent : 'space-between',
+         }}> 
      
             <View style = {{height : 50,width:50 , backgroundColor: 'powderblue'}}/>
             <View style = {{height : 50,width:50 , backgroundColor: 'skyblue'}}/>
