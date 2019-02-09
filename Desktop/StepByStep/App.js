b3f46af00186d4330eea2c43cb400974d8ccf5df
javadjafariv94 <javadjafariv94@gmail.com>
2019-02-09 18:19:26 +0330

    Height and Width

--- Desktop/StepByStep/App.js
+++ Desktop/StepByStep/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
 import { View,Text,StyleSheet} from 'react-native';
 
-const mystyles = StyleSheet.create({
-  bigBlue:{
-    color:'blue',
-    fontWeight :'bold',
-    fontSize : 30,
-  },
-  red:{
-    color :'red',
-  },  
-
-  });
-
 export default class App extends Component {
  render(){
     
     return (
       <View>
-      <Text style = {mystyles.red}> Are you ok ? </Text>
-      <Text style = {mystyles.bigBlue}> Are you ok ? </Text>
-      <Text style = { [mystyles.bigBlue , mystyles.red]  }> Are you ok ? </Text>
-      <Text style = { [mystyles.red , mystyles.bigBlue]  }> Are you ok ? </Text>
+            <View style = {{width : 50  ,height :50 , backgroundColor: 'powderblue'}}/>
+            <View style = {{width : 100 ,height :100 ,backgroundColor: 'skyblue'}}/>
+            <View style = {{width : 150 ,height :150 ,backgroundColor: 'steelblue'}}/>
      </View>
     );
   }
