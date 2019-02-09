abc082a4d91c565c902c0d626d80f1a169b25b7e
javadjafariv94 <javadjafariv94@gmail.com>
2019-02-09 17:47:28 +0330

    style

--- Desktop/StepByStep/App.js
+++ Desktop/StepByStep/App.js
@@ -1,35 +1,27 @@
 import React, { Component } from 'react';
-import { Image ,View,Text} from 'react-native';
+import { View,Text,StyleSheet} from 'react-native';
 
-class Blink extends Component{
-constructor(props){
- super(props);
- this.state ={ isShowingText :true };
+const mystyles = StyleSheet.create({
+  bigBlue:{
+    color:'blue',
+    fontWeight :'bold',
+    fontSize : 30,
+  },
+  red:{
+    color :'red',
+  },  
 
- setInterval(() => (this.setState(previosState11 =>({isShowingText : !previosState11.isShowingText})) ),1000);
-}
+  });
 
-render(){
-if (!this.state.isShowingText ) { return null; }
-
-return(
-  <View>  
-    <Text>{this.props.text} </Text>
-  </View>
-);
-}
-
-
-}
 export default class App extends Component {
  render(){
     
     return (
       <View>
-      <Blink text=' I love to blink 0' />
-      <Blink text=' I love to blink  1' />
-      <Blink text=' I love to blink   2' />
-      <Blink text=' I love to blink    3' />
+      <Text style = {mystyles.red}> Are you ok ? </Text>
+      <Text style = {mystyles.bigBlue}> Are you ok ? </Text>
+      <Text style = { [mystyles.bigBlue , mystyles.red]  }> Are you ok ? </Text>
+      <Text style = { [mystyles.red , mystyles.bigBlue]  }> Are you ok ? </Text>
      </View>
     );
   }
