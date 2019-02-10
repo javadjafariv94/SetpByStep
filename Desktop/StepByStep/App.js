5cb7960d480bdb53ff49a2ed059d4dd6764a7bd8
javadjafariv94 <javadjafariv94@gmail.com>
2019-02-10 17:43:21 +0330

    button Basic

--- Desktop/StepByStep/App.js
+++ Desktop/StepByStep/App.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
-import { Alert,Button, View } from 'react-native';
+import { Alert,Button,StyleSheet, View } from 'react-native';
 
-export default class PizzaTranslator extends Component {
+export default class App extends Component {
+  _onPressButton(){
+    Alert.alert('You tapped the button!')
+  } 
 
 
   render() {
     return (
-      <View>
-      <Button onPress = {
-            () => {Alert.alert('You tapped the button!'); }}
-            title ="Press me"
-            />
-       </View>
-      
-    );
+      <View style = {styles.container}>
+        <View style ={styles.buttonContainer}>
+          <Button 
+            onPress ={this._onPressButton}
+            title = 'Press Me1'
+          />
+        </View>
+        <View style ={styles.buttonContainer}>
+          <Button 
+            onPress ={this._onPressButton}
+            title = 'Press Me2'
+            color="#841584"
+          />
+        </View>
+
+        <View style ={styles.alter}>
+          <Button 
+            onPress ={this._onPressButton}
+            title = 'Press Me3'
+          />
+          <Button 
+            onPress ={this._onPressButton}
+            title = 'Press Me4'
+            color="#841584"
+          />
+        </View>
+
+
+
+      </View>
+     );
   }
 }
+
+const styles = StyleSheet.create(
+  {
+    container :{
+      flex :1,
+      justifyContent : 'center',
+
+    },
+    buttonContainer :{
+      margin :30
+    },
+    alter :{
+      margin :20,
+      flexDirection :'row',
+      justifyContent : 'space-between'
+    }
+  }
+);
