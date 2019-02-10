92acb0e4487a9d39c54d5e882975bd9e80a1790c
javadjafariv94 <javadjafariv94@gmail.com>
2019-02-10 16:54:11 +0330

    Handle Touch

--- Desktop/StepByStep/App.js
+++ Desktop/StepByStep/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Alert,Button, View } from 'react-native';
 
 export default class PizzaTranslator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
+
 
   render() {
     return (
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map(  (word) => word && '🍕').join(' ')}
-        </Text>
-          <Text style= {{padding:10 , fontSize: 42}}> {this.state.text} </Text>
-        </View>
+      <View>
+      <Button onPress = {
+            () => {Alert.alert('You tapped the button!'); }}
+            title ="Press me"
+            />
+       </View>
+      
     );
   }
 }
