996ffa5948eaf56e09ee538e7c25d3b981454758
javadjafariv94 <javadjafariv94@gmail.com>
2019-02-12 12:01:19 +0330

    FlatList

--- Desktop/StepByStep/App.js
+++ Desktop/StepByStep/App.js
@@ -1,41 +1,52 @@
 import React, { Component } from 'react';
 import { StyleSheet,Text,
-         SectionList,
+         FlatList,
          View } from 'react-native';
 
 export default class App extends Component {
  
   render() {
     return (
-      <View style={styles.container}>
-        <SectionList
-          sections={[
-            {title: 'D.....', data: ['Devin']},
-            {title: 'J.....', data: ['Jackson', 'Janson', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+      <View style = {styles.container}>
+        <FlatList
+          data ={[
+            {key :'Ali'},
+            {key :'Mohammad'},
+            {key :'Fateme'},
+            {key :'Hasan'},
+            {key :'Hosein'},
+            {key :'Ali'},
+            {key :'Mohammad'},
+            {key :'Fateme'},
+            {key :'Hasan'},
+            {key :'Hosein'},
+            {key :'Ali'},
+            {key :'Mohammad'},
+            {key :'Fateme'},
+            {key :'Hasan'},
+            {key :'Hosein'},
+            {key :'Ali'},
+            {key :'Mohammad'},
+            {key :'Fateme'},
+            {key :'Hasan'},
+            {key :'Hosein'},
+            {key :'Ali'},
+            {key :'Mohammad'},
+            {key :'Fateme'},
+            {key :'Hasan'},
+            {key :'Hosein'},
           ]}
-          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-          keyExtractor={(item, index) => index}
+          renderItem={({item}) => <Text style = {styles.item}> {item.key} </Text>}
         />
       </View>
     );
   }
 }
 
-
 const styles = StyleSheet.create({
   container: {
    flex: 1,
    paddingTop: 22
-  },
-  sectionHeader: {  
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
   },
   item: {
     padding: 10,
@@ -43,3 +54,4 @@ const styles = StyleSheet.create({
     height: 44,
   },
 })
+
