de6774df907f5756942d276c79ee38dd2069ebfd
javadjafariv94 <javadjafariv94@gmail.com>
2019-02-12 11:32:30 +0330

    sectionList

--- Desktop/StepByStep/App.js
+++ Desktop/StepByStep/App.js
@@ -1,49 +1,45 @@
 import React, { Component } from 'react';
-import { Alert, Platform, StyleSheet,Text,
-         ScrollView,Image,
+import { StyleSheet,Text,
+         SectionList,
          View } from 'react-native';
 
 export default class App extends Component {
  
   render() {
     return (
-      <ScrollView>
-        
-        <Text style={{fontSize:96}}>Scroll me plz 1</Text>
-        <Text style={{fontSize:96}}>Scroll me plz 2</Text>
-        <Text style={{fontSize:96}}>Scroll me plz 3</Text>
-        <Text style={{fontSize:96}}>Scroll me plz 4</Text>
-        <Text style={{fontSize:96}}>Scroll me plz 5</Text>
-        
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-         
-        <Text style={{fontSize:30}}>Scroll me plz 10</Text>
-        <Text style={{fontSize:30}}>Scroll me plz 20</Text>
-        <Text style={{fontSize:30}}>Scroll me plz 30</Text>
-        <Text style={{fontSize:30}}>Scroll me plz 40</Text>
-        <Text style={{fontSize:30}}>Scroll me plz 50</Text>
-      </ScrollView>
-     );
+      <View style={styles.container}>
+        <SectionList
+          sections={[
+            {title: 'D.....', data: ['Devin']},
+            {title: 'J.....', data: ['Jackson', 'Janson', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+    );
   }
 }
 
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 60,
-    alignItems: 'center'
+   flex: 1,
+   paddingTop: 22
   },
-  button: {
-    marginBottom: 30,
-    width: 260,
-    alignItems: 'center',
-    backgroundColor: '#2196F3'
+  sectionHeader: {  
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
   },
-  buttonText: {
-    padding: 20,
-    color: 'white'
-  }
-});
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
