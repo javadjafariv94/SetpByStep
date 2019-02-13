a247c698fd37c28caf60f9e1b6a8298022c3f14a
javadjafariv94 <javadjafariv94@gmail.com>
2019-02-13 12:21:16 +0330

    Home screens full

--- Desktop/StepByStep/App.js
+++ Desktop/StepByStep/App.js
@@ -1,31 +1,107 @@
-import * as React from 'react';
-import { View } from 'react-native';
-import {
-  createAppContainer,
-  createBottomTabNavigator,
-  createStackNavigator,
-} from 'react-navigation';
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
 
-const ExampleScreen = View;
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
 
-const Home = createStackNavigator(
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => {
+            /* 1. Navigate to the Details route with params */
+            this.props.navigation.navigate('Details', {
+              itemId: 86,
+              otherParam: 'anything you want here',
+            });
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  static navigationOptions = ({ navigation, navigationOptions }) => {
+    console.log(navigationOptions);
+    // Notice the logs ^
+    // sometimes we call with the default navigationOptions and other times
+    // we call this with the previous navigationOptions that were returned from
+    // this very function
+    return {
+      title: navigation.getParam('otherParam', 'A Nested Details Screen'),
+      headerStyle: {
+        backgroundColor: navigationOptions.headerTintColor,
+      },
+      headerTintColor: navigationOptions.headerStyle.backgroundColor,
+    };
+  };
+
+  render() {
+    /* 2. Get the param, provide a fallback value if not available */
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID'); 
+    const otherParam = navigation.getParam('otherParam', 'some default value');
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() =>
+            this.props.navigation.push('Details', {
+              itemId: Math.floor(Math.random() * 100),
+            })}
+        />
+        <Button
+          title="Update the title"
+          onPress={() =>
+            this.props.navigation.setParams({ otherParam: 'Updated!' })}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
   {
-    Feed: ExampleScreen,
-    Profile: ExampleScreen,
+    Home: HomeScreen,
+    Details: DetailsScreen,
   },
   {
+    initialRouteName: 'Home',
+    /* The header config from HomeScreen is now here */
     defaultNavigationOptions: {
-      title: 'Home',
-      headerTintColor: '#fff',
       headerStyle: {
-        backgroundColor: '#000',
+        backgroundColor: '#f4511e',
       },
-    },
-    navigationOptions: {
-      tabBarLabel: 'Home!',
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
     },
   }
 );
 
-const Tabs = createBottomTabNavigator({ Home });
-export default createAppContainer(Tabs);
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
