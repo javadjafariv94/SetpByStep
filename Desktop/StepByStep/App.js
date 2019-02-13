9f50c3152acb6855ba4a0583ed50238be84167ac
javadjafariv94 <javadjafariv94@gmail.com>
2019-02-13 12:16:27 +0330

    Home screens

--- Desktop/StepByStep/App.js
+++ Desktop/StepByStep/App.js
@@ -1,98 +1,31 @@
-import React from 'react';
-import { Button, View, Text } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
+import * as React from 'react';
+import { View } from 'react-native';
+import {
+  createAppContainer,
+  createBottomTabNavigator,
+  createStackNavigator,
+} from 'react-navigation';
 
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Home',
-  };
+const ExampleScreen = View;
 
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            this.props.navigation.navigate('Details', {
-              itemId: 86,
-              otherParam: 'anything you want here',
-            });
-          }}
-        />
-      </View>
-    );
-  }
-}
-
-class DetailsScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => {
-    return {
-      title: navigation.getParam('otherParam', 'A Nested Details Screen'),
-    };
-  };
-
-  render() {
-    /* 2. Get the param, provide a fallback value if not available */
-    const { navigation } = this.props;
-    const itemId = navigation.getParam('itemId', 'NO-ID');
-    const otherParam = navigation.getParam('otherParam', 'some default value');
-
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>itemId: {JSON.stringify(itemId)}</Text>
-        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() =>
-            this.props.navigation.push('Details', {
-              itemId: Math.floor(Math.random() * 100),
-            })}
-        />
-        <Button
-          title="Update the title"
-          onPress={() =>
-            this.props.navigation.setParams({ otherParam: 'Updated!' })}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
-    );
-  }
-}
-
-const RootStack = createStackNavigator(
+const Home = createStackNavigator(
   {
-    Home: HomeScreen,
-    Details: DetailsScreen,
+    Feed: ExampleScreen,
+    Profile: ExampleScreen,
   },
   {
-    initialRouteName: 'Home',
-    /* The header config from HomeScreen is now here */
     defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: '#f4511e',
-      },
+      title: 'Home',
       headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
+      headerStyle: {
+        backgroundColor: '#000',
       },
+    },
+    navigationOptions: {
+      tabBarLabel: 'Home!',
     },
   }
 );
 
-const AppContainer = createAppContainer(RootStack);
-
-export default class App extends React.Component {
-  render() {
-    return <AppContainer />;
-  }
-}
+const Tabs = createBottomTabNavigator({ Home });
+export default createAppContainer(Tabs);
